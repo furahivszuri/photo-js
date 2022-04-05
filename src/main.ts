1b3ecc316dbce6dfeeac33fff591be4e00c87a59
FURAHI-SUNG\zuri <furahisung@3bassets.com>
2022-04-04 22:40:53 -0700

    Change console based Hello World into express based Hello World

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-function helloWorld() {
-    console.log("Hello World");
-}
+import express from 'express'
+const app = express()
+const port = 3000
 
-helloWorld();
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
